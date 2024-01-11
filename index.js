8ced90221e9dc0fca1ccd2e1ce57217739b05f94
akhileshsoni <akhileshsoni098@gmail.com>
2024-01-11 21:04:04 +0530

    kkkkkkk

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-require("dotenv").config();
+/* require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-
-// const requestIp = require("request-ip");
-// const axios = require("axios");
 
 const app = express();
 
@@ -27,6 +24,39 @@ app.get("/", async (req, res) => {
 console.log(ip)
 
         res.json({status:true , message:'sucessFully got ip', userIP:ip});
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ message: "Something went wrong", status: 500 });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
+ */
+
+
+
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const requestIp = require("request-ip");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+app.use(requestIp.mw());
+
+app.get("/", async (req, res) => {
+    try {
+        const userIP = req.clientIp;
+        // console.log(userIP)
+
+        res.json({ status: true, message: 'Successfully got IP', userIP });
     } catch (error) {
         console.error("Error:", error);
         res.status(500).json({ message: "Something went wrong", status: 500 });
