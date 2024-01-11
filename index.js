4c0cf0ae901b01bf11d3469f8310ce9250d154dc
akhileshsoni <akhileshsoni098@gmail.com>
2024-01-11 17:41:58 +0530

    kkkk

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+// const requestIp = require("request-ip");
+// const axios = require("axios");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json())
+
+app.use(cors({ origin: "*" }));
+
+
+
+app.get("/", async (req, res) => {
+    try {
+        const ip = 
+        req.headers['cf-connecting-ip'] ||  
+        req.headers['x-real-ip'] ||
+        req.headers['x-forwarded-for'] ||
+        req.socket.remoteAddress || '';
+
+console.log(ip)
+
+        res.json({status:true , message:'sucessFully got ip', userIP:ip});
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ message: "Something went wrong", status: 500 });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
