01ab2ad18256e6b0f37b37538ac7a281bc9c7b58
akhileshsoni <akhileshsoni098@gmail.com>
2024-01-11 21:21:54 +0530

    kkkk

--- index.js
+++ index.js
@@ -36,7 +36,9 @@ app.listen(port, () => {
  */
 
 
+///////////////////////////////////////////////////
 
+/* 
 require("dotenv").config();
 
 const express = require("express");
@@ -77,3 +79,50 @@ app.listen(port, () => {
 
 
 
+ */
+
+
+
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const requestIp = require("request-ip");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+app.use(requestIp.mw());
+
+app.get("/", async (req, res) => {
+    try {
+        const publicIP = req.clientIp;
+        const localIP = getClientLocalIP(req);
+
+        res.json({
+            status: true,
+            message: 'Successfully got IPs',
+            publicIP,
+            localIP,
+        });
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ message: "Something went wrong", status: 500 });
+    }
+});
+
+
+function getClientLocalIP(req) {
+    const ipv4 = req.connection.remoteAddress;
+    const ipv6 = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+
+    
+    return ipv4.includes('::1') ? ipv6 : ipv4;
+}
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
